cf6345c8b44461b93ba30247d0478b9c0fc5b7c4
kavisliit <it20009540@my.sliit.lk>
2023-03-10 21:53:39 +0530

    Moview Review changes

---
+++ src/common/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, TextInput, Button, Alert, Pressable, Image, Text } from "react-native";
+
+function Home(props) {
+    return (
+        <View>
+            <Text>
+                sss
+            </Text>
+        </View>
+    );
+}
+
+export default Home;
