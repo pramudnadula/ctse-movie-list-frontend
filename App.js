7da6961a49d08b625814cbaf794d841216802c52
ckbgit <it20274498@my.sliit.lk>
2023-03-13 23:44:35 +0530

    all movies

--- App.js
+++ App.js
@@ -3,8 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/common/Home';
-import Login from './src/common/Login';
-import ReviewList from './src/features/movie_review/pages/ReviewList';
+//import Login from './src/common/Login';
+//import ReviewList from './src/features/movie_review/pages/ReviewList';
+import AddMovie from './src/features/userMovie/pages/AddMovie';
+import AllMovies from './src/features/userMovie/pages/AllMovies';
+
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -16,7 +19,9 @@ export default function App() {
             headerShown: false
           }
         }>
-        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Login" component={Login} /> */}
+        <Stack.Screen name="AddMovie" component={AddMovie} />
+        <Stack.Screen name="AllMovie" component={AllMovies} />
       </Stack.Navigator>
     </NavigationContainer>
   );
